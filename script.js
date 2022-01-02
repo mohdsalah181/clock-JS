0a321d7f3b6106f8d8b080fede54b999296614ca
unknown <Administrator@TECH746.mtn.sd>
2022-01-02 22:45:32 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+    setInterval(showTime, 1000);
+    
+}
+
+
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+
+    h =  (h == 0) ? 12 : h-12;
+    session = (h > 12) ? "PM" : "AM"
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var  time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+}
